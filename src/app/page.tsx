11ed7c76e5e654288f1a590f60d4b518513ec165
AriunbaatarG <g.ariunbaatar861@gmail.com>
2023-11-17 16:28:59 +0100

    UI design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-10">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Martina Demaj Portfolio&nbsp;
         </p>
@@ -18,36 +18,65 @@ export default function Home() {
           </a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="grid grid-cols-3">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/1.JPG"
+          className="object-fill m-2"
+          src="/Editorials/DSC_1834.JPG"
           alt="Martina main"
           width={300}
           height={200}
           priority
         />
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/2.JPG"
+          className="object-fill m-2"
+          src="/Editorials/IMG_1378.JPG"
           alt="Martina main"
           width={300}
           height={200}
           priority
         />
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="object-fill m-2"
+          src="/Editorials/DSC_9970.jpg"
+          alt="Martina main"
+          width={300}
+          height={200}
+          priority
+        />
+        <Image
+          className="object-fill m-2"
+          src="/Event Photography/Balet Show/image00009.jpeg"
+          alt="Martina main"
+          width={300}
+          height={200}
+          priority
+        />
+        <Image
+          className="object-fill m-2"
+          src="/Editorials/IMG_8328.JPG"
+          alt="Martina main"
+          width={300}
+          height={200}
+          priority
+        />
+        <Image
+          className="object-fill m-2"
+          src="/Event Photography/Balet Show/image00011.jpeg"
+          alt="Martina main"
+          width={300}
+          height={200}
+          priority
+        />
+        <Image
+          className="object-fill m-2"
           src="/3.JPG"
           alt="Martina main"
           width={300}
           height={200}
           priority
         />
-      </div>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="object-fill m-2"
           src="/4.JPG"
           alt="Martina main"
           width={300}
@@ -55,7 +84,7 @@ export default function Home() {
           priority
         />
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="object-fill m-2"
           src="/5.JPG"
           alt="Martina main"
           width={300}
@@ -63,23 +92,41 @@ export default function Home() {
           priority
         />
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/6.JPG"
+          className="object-fill m-2 grid-col-2"
+          src="/Event Photography/Spotlight 2023/image00004.jpeg"
+          alt="Martina main"
+          width={300}
+          height={200}
+          priority
+        />
+        <Image
+          className="object-fill m-2 grid-col-2"
+          src="/Event Photography/Dancestoned 2023/IMG_4550.JPG"
+          alt="Martina main"
+          width={300}
+          height={200}
+          priority
+        />
+        <Image
+          className="object-fill m-2 grid-col-2"
+          src="/Event Photography/Dancestoned 2023/IMG_4606.JPG"
           alt="Martina main"
           width={300}
           height={200}
           priority
         />
       </div>
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <a
-          href= "/page.tsx"
+          href= "src/app/landscape.tsx"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Landscape{' '}
+          <h2 className={`mb-3 text-xl font-semibold`}>
+            Landscape Photography{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -95,7 +142,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className={`mb-3 text-xl font-semibold`}>
             Editorial Photography{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
@@ -112,7 +159,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
+          <h2 className={`mb-3 text-xl font-semibold`}>
             Event Photography{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
@@ -120,23 +167,6 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Large performances, weddings, events
-          </p>
-        </a>
-
-        <a
-          href="/page.tsx"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Prints{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Prints
           </p>
         </a>
       </div>
