5807074a0ba6637e58f256b92596cd63dbda0d05
AriunbaatarG <g.ariunbaatar861@gmail.com>
2023-11-18 13:33:55 +0100

    name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Martinaiden Portfolio',
+  title: 'IDENLESSLY',
   description: 'Photography Portfolio of Martinaiden Demaj',
 }
 
