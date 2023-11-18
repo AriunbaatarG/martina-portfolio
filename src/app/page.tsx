b70247eb05726c8c38a1f84ce47d52e77a3cf61e
AriunbaatarG <g.ariunbaatar861@gmail.com>
2023-11-18 13:46:56 +0100

    design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,10 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex mb-10">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex mb-10">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30">
           Martinaiden Demaj Portfolio&nbsp;
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"> 
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://www.instagram.com/idenlesssly/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Contact
-          </a>
-        </div>
       </div>
       <div className="mb-8 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <Link href= "/event_photography" passHref legacyBehavior> 
@@ -155,7 +145,16 @@ export default function Home() {
           priority
         />
       </div>
-
+      <div className=" bottom-0 left-0 mt-8 flex w-full items-end justify-center dark:from-black dark:via-black text-lg"> 
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 pointer-events-auto lg:p-0"
+            href="https://www.instagram.com/idenlessly/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contact
+          </a>
+        </div>
 
       
     </main>
